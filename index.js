d2c992108e981ade34ba47a20773327c8260d2dc
belkud <vlad-belkud@yandex.ru>
2024-01-22 12:54:29 +0300

    csdd

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 
 const http = require ('http')
+const fs = require ('fs')
 
 
 const html = `
@@ -13,30 +14,35 @@ const html = `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
-</head>
-
+    <link rel="icon" href="/public/jspicture.jpg" sizes: "64*64" type="image/jpg">
+    
+    </head>
+    
 <style>
-body {
-    background-color:gray;
-  
-}
-h1 {
-    text-align:center;
-}
-#open {
-    color:red;
-}
-div {
-    margin:20px;
-}
+    @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital@0;1&display=swap');
+
+    body {
+        background-color:gray;
+        font-family: 'Playfair Display', serif;
+    }
+    h1 {
+        text-align:center;
+    }
+    #open {
+        color:red;
+    }
+    div {
+        margin:20px;
+    }
 
 </style>
 
+
 <body>
     <h1>Мой первый сервер</h1>
-    <div style="color:red">hettt</div>
     <button id="open">Открыть</button>
+    <div style="color:red">hettt</div>
+
 
     <script>
         const button = document.querySelector('#open')
@@ -51,15 +57,24 @@ div {
     //! <link rel="stylesheet" href="style.css">
 
 const port = 3000
+function staticFile () {
+
+}
+
 
 http.createServer((req,res)=> {
     switch (req.url) {
         case '/':
             res.writeHead (200, {'Content-Type': 'text/html'});
-            res.write(html) 
-            res.write('<p>hello world</p> <br>')
+            let data = fs.readFileSync('./addInfo.html', {encoding: 'utf-8', flag:"r"})
+            res.write(`${html} ${data}`)
+            // res.write(' <br>')
+        
+            
             res.end('main page')
-         
+
+
+
         case '/second':
             res.writeHead (200, {'Content-Type': 'text/html'});
             res.write(html)   
